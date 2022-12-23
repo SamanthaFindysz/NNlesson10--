8677b455f24bdf73aeac6509f96b9beb2e323df4
SamanthaFindysz <113720736+SamanthaFindysz@users.noreply.github.com>
2022-12-23 17:44:40 -0600

    commit Ninjas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+/**this is the root component. 
+ * React apps typically have one root component
+ * that's always rendered to the DOM.
+ * It then accesses other components
+ * to render them to the DOM (nested)
+ */
 
+import Ninjas from './Ninjas';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My first React app!</h1>
+      <p>Welcome!</p>
+      <Ninjas />
     </div>
   );
 }
---
+++ src/Ninjas.js
@@ -0,0 +1,13 @@
+//import React, {Component} from 'react';
+
+const Ninjas = () =>{
+    return(
+        <div>
+            <div>Name: Ryu</div>
+            <div>Age: 30</div>
+            <div>Belt: Black</div>
+        </div>
+    )
+}
+
+export default Ninjas
